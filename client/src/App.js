bde56151ec39f3d41f4221630d9d40d152bab40e
Josh <zylo.codes@gmail.com>
2021-04-03 12:00:16 -0500

    Router, Nav, Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Draw from './pages/draw'
+import Home from './pages/home'
+import Nav from './components/nav'
+
 function App() {
   return (
-    <div>App.js</div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/draw">
+          <Draw />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/draw.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Draw = () => (
+  <div>Draw</div>
+)
+
+export default Draw
