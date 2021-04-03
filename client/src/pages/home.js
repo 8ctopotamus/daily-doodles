bb31c1d1b7b238afcc60fcbe30dfb23a74f12ec2
Josh <zylo.codes@gmail.com>
2021-04-03 12:43:18 -0500

    Home layout and DrawingCard

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import API from '../utils/API'
+import Container from '../components/container'
+import Row from '../components/row'
+import Col from '../components/col'
+import DrawingCard from '../components/drawing-card'
 
-const Home = () => (
-  <div>HOME</div>
-)
+const Home = () => {
+  const [drawings, setDrawings] = useState([])
+  console.log(drawings)  
+
+  useEffect(() => {
+    API.getDrawings()
+      .then(response => setDrawings(response.data))
+      .catch(err => console.log(err))
+  }, [])
+  
+  return (
+    <Container>
+      <Row>
+        { drawings.map(drawing => (
+          <Col className="col-md-4" key={drawing._id}>
+            <DrawingCard {...drawing} />
+          </Col>
+        )) }
+      </Row>
+    </Container>
+  )
+}
 
 export default Home
---
+++ client/src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export default {
+  getDrawings: function() {
+    return axios('/api/drawings')
+  }
+}
