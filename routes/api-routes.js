e9fce4e5c8c11f592cd1f2504486c4b9c6ac9782
Josh <zylo.codes@gmail.com>
2021-04-03 11:43:50 -0500

    basic DrawingController

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,13 +2,14 @@ const router = require('express').Router()
 const drawingController = require('../controllers/drawingController')
 const userController = require('../controllers/userController')
 
+router.route('/api/drawings/:id')
+  .get(drawingController.getDrawing)
+  .put(drawingController.updateDrawing)
+  .delete(drawingController.deleteDrawing)
+
 router.route('/api/drawings')
   .get(drawingController.getDrawings)
   .post(drawingController.createDrawing)
-
-router.route('/api/drawings/:id')
-  .put(drawingController.updateDrawing)
-  .delete(drawingController.deleteDrawing)
 
 router.route('/api/users/:id')
   .get(userController.getUser)
