4621cff706ea33845b066a8ea6f2380cb389a301
Josh <zylo.codes@gmail.com>
2021-04-03 11:27:42 -0500

    UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,24 @@ const db = require('../models')
 module.exports = {
   getUser: function(req, res) {
     const { id } = req.params
-    console.log(id)
-    res.send(id)
+    db.User.findById(id)
+      .then(userData => {
+        res.json(userData)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).send()
+      })
   },
   updateUser: function(req, res) {
-
+    const { id } = req.params
+    db.User.findByIdAndUpdate(id, req.body, { new: true })
+      .then(userData => {
+        res.json(userData)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).send()
+      })
   }
 }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose')
 const express = require("express");
 const path = require("path");
 const apiRoutes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dailydoodles");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
