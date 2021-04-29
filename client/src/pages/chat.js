b7ba7fac334e17040ff1a04f973a53e37d4b74aa
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-29 14:32:14 -0500

    basic socket communication is happening

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -3,15 +3,25 @@ import API from '../utils/API'
 import Container from '../components/container'
 import Row from '../components/row'
 import Col from '../components/col'
-import DrawingCard from '../components/drawing-card'
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:3000";
 
 const Chat = () => {
-    console.log('chatload')
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        console.log(socket)
+        socket.on("FromAPI", data => {
+            setResponse(data);
+        });
+    }, []);
+
     return (
         <Container>
             <Row>
-                CHAT NOW
-        
+                static vs {response}
+
             </Row>
         </Container>
     )
--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const path = require("path");
 const apiRoutes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
+const cors = require('cors')
+const morgan = require('morgan')
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dailydoodles");
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const ioOptions = {
+  cors: true,
+  origins: ["http://127.0.0.1:3000"],
+}
+const io = new Server(server, ioOptions);
+//io.set('origins', 'localhost:3000');
+
+//app handles routes
+//app is used to create (http)server
+//(http)server is used to create socket.io Server
+
+io.on('connection', (socket) => {
+  socket.emit("FromAPI", "This is API speaking")
+  console.log('a user connected', socket);
+});
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('dev'))
+app.use(cors())
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -25,6 +48,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
