ca019e46ba0cdcd33d8930896bfe6fbfdd389ea0
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-29 13:59:11 -0500

    added place to gradually implement socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import Draw from './pages/draw'
 import Home from './pages/home'
+import Chat from './pages/chat'
 import Nav from './components/nav'
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
     <Router>
       <Nav />
       <Switch>
+        <Route path="/chat">
+          <Chat />
+        </Route>
         <Route path="/draw">
           <Draw />
         </Route>
---
+++ client/src/pages/chat.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import API from '../utils/API'
+import Container from '../components/container'
+import Row from '../components/row'
+import Col from '../components/col'
+import DrawingCard from '../components/drawing-card'
+
+const Chat = () => {
+    console.log('chatload')
+    return (
+        <Container>
+            <Row>
+                CHAT NOW
+        
+            </Row>
+        </Container>
+    )
+}
+
+export default Chat
